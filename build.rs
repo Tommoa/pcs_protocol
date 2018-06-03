8db464295ebd1ffee5aa9c8504e5ca549cba0705
Tom Almeida <tommoa256@gmail.com>
2018-06-04 01:01:31 +0800

    Revert "Moved from custom implementation of protocol to using protobuf"
    This reverts commit ea785f988f8bdb75fa9f5ff1a7217d480bb5b8fe.
    
    Turns out that this change was actually a bad idea. It forced a lot of overhead onto our spec, so we're returning to the old spec and reimplementing the de/serializer instead.

--- build.rs
+++
@@ -1,9 +0,0 @@
-extern crate protoc_rust;
-
-fn main() {
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "src",
-        input: &["pcs.proto"],
-        includes: &[],
-    }).expect("protoc");
-}
